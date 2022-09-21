696382a62ad152daa28ad81ce652cbc4399bec56
1998arunrekha <arunar16061998@gmail.com>
2022-09-21 17:31:56 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+module "Resourcegroup" {
+    source ="./Resourcegroup"
+    resourcegroup = "myrg335"
+    location = "east us"
+
+  
+}
